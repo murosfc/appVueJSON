6cdfa75f8fedf61b3a5ff2e669a152cb2c77f479
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-04-22 20:13:29 -0300

    Finalizada a adição, edição e exclusão de jogos

--- my-app/src/pages/GamesCrud.vue
+++ my-app/src/pages/GamesCrud.vue
@@ -1,22 +1,9 @@
 <template>
     <v-container>             
-        <v-row class="table-title">
+        <v-row class="table-title" data-app>
             <v-col cols="12">
                 <h1>{{titulo}}</h1>
-            </v-col>
-            <v-col>
-                <v-btn
-                class="mx-2"
-                fab
-                dark
-                small
-                color="indigo"
-                >
-                <v-icon dark>
-                    mdi-plus
-                </v-icon>
-            </v-btn>
-            </v-col>
+            </v-col>      
         </v-row>
         <v-row class="table-games">
             <v-col cols="12">
@@ -25,7 +12,97 @@
                     <a :href="value">
                         link
                     </a>
-                </template>              
+                </template>                 
+                 ##Formulario
+                <template v-slot:top>
+                    <v-toolbar flat>
+                    <v-toolbar-title>Jogos cadastrados</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>                    
+                    <v-spacer></v-spacer>
+                    <v-dialog v-model="dialog" max-width="700px" max-length="500px">
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-btn 
+                            class="mx-2"
+                            fab
+                            dark
+                            small
+                            color="indigo"
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                        <v-icon dark>
+                            mdi-plus
+                        </v-icon>
+                        </v-btn>
+                        </template>
+                        <v-card>
+                        <v-card-title>
+                            <span class="headline">Adicionar/Editar Jogo</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                            <v-row>
+                                <v-col cols="12" sm="6" md="4">
+                                <v-text-field
+                                    v-model="editedItem.id"
+                                    label="Id"                                    
+                                ></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="8">
+                                <v-text-field
+                                    v-model="editedItem.titulo"
+                                    label="Título"
+                                ></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                <v-text-field
+                                    v-model="editedItem.plataforma"
+                                    label="Plataforma"
+                                ></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                <v-text-field
+                                    v-model="editedItem.urlimg"
+                                    label="URL da imagem"
+                                ></v-text-field>
+                                </v-col>
+                                 <v-col cols="12" sm="6" md="8">
+                                <v-text-field
+                                    v-model="editedItem.valor"
+                                    label="Valor do aluguel"
+                                ></v-text-field>
+                                </v-col>
+                                
+                            </v-row>
+                            </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="close">
+                            Cancel
+                            </v-btn>
+                            <v-btn color="blue darken-1" text @click="save">
+                            Save
+                            </v-btn>
+                        </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                    </v-toolbar>
+                </template>
+                ##Fim Form 
+                <template v-slot:[`item.tipo_id`]="{ item }">
+                    {{ descricao(item.tipo_id) }}
+                </template>
+
+                <template v-slot:[`item.actions`]="{ item }">
+                    <v-icon small class="mr-2" @click="editItem(item)">
+                        mdi-pencil
+                    </v-icon>
+                    <v-icon small @click="deleteItem(item)">
+                        mdi-delete 
+                    </v-icon>
+                </template>          
                 </v-data-table>                              
             </v-col>
         </v-row>
@@ -39,26 +116,79 @@ export default ({
     data: () => {
         return {
             titulo: "Edição de banco de dados de jogos",
+            search: "",
+            dialog: false,
             headers: [
                 {text: "Id", value: "id"},
                 {text: "Título", value: "titulo"},
                 {text: "Plataforma", value: "plataforma"},
                 {text: "Valor", value: "valor"},
                 {text: "Imagem", value: "urlimg"},
-                {text: "Editar"}
+                { text: "Actions", value: "actions", sortable: false }                
             ],
-            jogos: []            
+            jogos: [],                       
+            editedItem: {id: "", titulo: "", plataforma: "", urlimg: "", valor: 20},
+            defaultItem: {id: "", titulo: "", plataforma: "", urlimg: "", valor: 20},
+            editedIndex: -1                  
         }           
     },
     methods: {
         inicializa() {
             axios("http://localhost:3000/games")
             .then((response)=> {
-                this.jogos = response.data;
-                console.log(this.jogos);
+                this.jogos = response.data;                
             })
-            .catch((error)=> console.log(error));
-        }
+            .catch((error)=> console.log(error));            
+        },
+        close(){
+            this.dialog = false
+            setTimeout(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;                
+            }, 300);
+        },
+        save() {
+            if (this.editedIndex > -1) {
+                //alteracao
+                axios
+                .put(
+                    "http://localhost:3000/games/" + this.editedItem.id,
+                    this.editedItem
+                )
+                .then((response) => {
+                    console.log(response);
+                    Object.assign(this.jogos[this.editedIndex], this.editedItem);
+                    this.close();
+                })
+                .catch((error) => console.log(error));
+            } else {
+                //Inclusao
+                axios
+                .post("http://localhost:3000/jogos", this.editedItem)
+                .then((response) => {
+                    console.log(response);
+                    this.jogos.push(this.editedItem);
+                    this.close();
+                })
+                .catch((error) => console.log(error));
+            }         
+        },
+        editItem(item) {
+        this.editedIndex = this.jogos.indexOf(item);
+        this.editedItem = Object.assign({}, item);
+        this.dialog = true;
+        },
+        deleteItem(item) {
+        const index = this.jogos.indexOf(item);
+        confirm("Deseja apagar este item?") &&
+            axios
+            .delete("http://localhost:3000/games/" + item.id)
+            .then((response) => {
+                console.log(response.data);
+                this.games.splice(index, 1);                
+            })
+            .catch((error) => console.log(error));
+        }       
     },
     created() {
         this.inicializa();
--- my-app/src/pages/home.vue
+++ my-app/src/pages/home.vue
@@ -1,26 +1,50 @@
 <template> 
   <v-container>
-    <h1>{{title}}</h1><br><br>
-    <v-row class="text-center">
+    <h2>{{title}}</h2><br><br>
+    <v-row class="logo">
       <v-col cols="12">
         <v-img
           :src="require('../assets/logoOngames.png')"
           class="my-3"
           contain
-          height="200"
+          height="150"
         />
       </v-col>
-    </v-row>
+    </v-row>  
+    <template>
+      <v-row class="jogos_disponiveis">
+        <v-col cols="12"  v-for="jogo in jogos" :key="jogo.id">      
+          <h3>{{jogo.titulo}}</h3>
+          <img scr={jogo.imgurl}/>
+          <h4>Plataforma: {{jogo.plataforma}}</h4>
+          <span>Valor do aluguel = R$ {{jogo.valor}}</span> 
+        </v-col>
+      </v-row>
+    </template>
    </v-container>
-    
 </template>
 <script>
-export default{
+import axios from "axios";
+export default ({
     name: "homePage",
-    data: () => ({ 
-        title: "Bem vindo à Ongames, sua locadora digital de jogos"           
-    }),
+    data: () => {
+      return { 
+        title: "Bem vindo à Ongames, sua locadora digital de jogos",
+        jogos: []         
+       }
+    },
     methods: {
+      inicializa() {
+        axios("http://localhost:3000/games")
+        .then((response)=> {
+            this.jogos = response.data;                
+        })
+        .catch((error)=> console.log(error));
+        console.log(this.jogos);            
+      },
+      created() {
+        this.inicializa();
+      } 
     }
-}
+})
 </script>
