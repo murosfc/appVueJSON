1919441d013097c26c2bfdc040480c9a8025c50c
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-04-29 22:13:59 -0300

    adicionado o carrinho ao home

--- my-app/src/pages/home.vue
+++ my-app/src/pages/home.vue
@@ -10,6 +10,15 @@
           height="150"
         />
       </v-col>
+    </v-row>
+    <v-row>
+      <div class="cart">
+      <label>Carrinho: </label>
+      <button 
+        type="button"
+        class="btn btn-primary" @click="verCarrinho()">
+        <v-icon>mdi-cart</v-icon>{{cart.length}}</button>
+      </div>    
     </v-row>  
     <template>
       <div class="flex-container">
@@ -17,14 +26,26 @@
           <h3>{{jogo.titulo}}</h3>
           <img :src="jogo.urlimg" class="game-pic"/>
           <h4>Plataforma: {{jogo.plataforma}}</h4>
-          <span>Valor do aluguel = R$ {{jogo.valor}}</span> 
+          <span>Valor do aluguel = R$ {{jogo.valor}}   </span>
+          <span>
+            <v-btn @click="addCart(jogo)" 
+            :disabled="clicked[jogo.id].status"
+            class="add-cart"
+            icon              
+            color="red"                                
+            >
+            <v-icon>mdi-cart-arrow-down</v-icon>
+          </v-btn>
+          </span>            
         </div>
-      </div>
-    </template>  
-  </v-container> 
+      </div> 
+    </template>    
+  </v-container>
+   
 </template>
 
 <script>
+
 import axios from "axios";
 require('@/assets/styles/estilo.css')
 export default ({
@@ -32,20 +53,47 @@ export default ({
     data: () => {
       return { 
         title: "Bem vindo à Ongames, sua locadora digital de jogos",
-        jogos: []         
+        jogos: [],
+        cart: [], 
+        clicked: [
+          {
+            "id": 0,
+            "status": false
+          }
+        ],        
        }
     },
     methods: {
       inicializa() {
         axios("http://localhost:3000/games")
         .then((response)=> {
-            this.jogos = response.data;                
+            this.jogos = response.data;            
+            for (var i=0;i<this.jogos.length;i++){
+              const obj = [];
+              obj['id']= i;
+              obj['status'] = false;
+              this.clicked.push(obj);
+            }                
         })                       
         .catch((error)=> console.log(error));                        
       },
-    },
+      addCart(jogo){    
+        this.clicked[jogo.id].status = true;
+        this.cart.push(Object.assign({}, jogo));          
+      },
+      verCarrinho(){
+        //adicionar método
+      },
+    },    
     created() {
       this.inicializa();
     }    
 })
 </script>
+<style scoped>
+   .cart{
+     margin-right: 0;
+     margin-left: auto;
+     margin-bottom: 20px;
+   }   
+</style>
